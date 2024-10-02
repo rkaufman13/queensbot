ee39ab4ff51744e2c3202c798d074a70eb57fd2b
rachel <16939591+rkaufman13@users.noreply.github.com>
2024-10-02 13:46:47 -0400

    add episode tracking

--- gameboard.py
+++ gameboard.py
@@ -1,85 +1,132 @@
-import gymnasium, time
-from gymnasium import error, spaces, utils
-from gymnasium.utils import seeding
-import random
-from points import get_points
+import gymnasium, collections, csv
+from gymnasium import error, spaces
+import tqdm
+from points import get_points, convert_matrix
 import numpy as np
 
 
 
 class QueensEnv(gymnasium.Env):
   metadata = {'render.modes': ['human']}
-  def __init__(self):
+  def __init__(self, length):
     super(gymnasium.Env, self).__init__()
-    self.game = np.zeros(shape=(8,8))
-    self.x = random.randint(0,7)
-    self.y = random.randint(0,7)
-    self.action_space = spaces.Discrete(5,)
-
+    self.length = length
+    self.game = np.zeros(shape=(length,length))
+    self.action_space = spaces.Discrete(length**2,)
   def step(self, action):
-    
-    reward = 1
     win = False
-  
-    if action==0:
-      value = self.game[self.y][self.x]
-      if value==0:
-        self.game[self.y][self.x]=1
-      else:
-        self.game[self.y][self.x]=0
-      reward+= get_points(self.game)[0]
-      win=get_points(self.game)[1]
-      
-    elif action==1:
-      self.y = self.clamp(self.y-1, 0, 7)
-      reward+= get_points(self.game)[0]
-      win=get_points(self.game)[1]
-    
-    elif action==2:
-      self.x = self.clamp(self.x+1,0,7)
-      reward+= get_points(self.game)[0]
-      win=get_points(self.game)[1]
-    
-    elif action==3:
-      self.y = self.clamp(self.y+1,0,7)
-      reward+= get_points(self.game)[0]
-      win=get_points(self.game)[1]
-
-    elif action==4:
-      self.x = self.clamp(self.x-1,0,7)
-      reward+= get_points(self.game)[0]
-      win=get_points(self.game)[1]
-    
-    return self.game,reward,win, []
-  def reset(self):
-    self.game = np.zeros(shape=(8,8))
-    self.x = random.randint(0,7)
-    self.y = random.randint(0,7)
-    return self.game
+    x = int(action/self.length)
+    y = action % self.length
+    self.game[y,x]=1
+    reward, win, lose, info = get_points(self.game, self.length)    
+    return self.game,reward,win, lose, info
+  def reset(self, seed=None, options=None):
+    self.game = np.zeros(shape=(self.length, self.length))
+    return self.game, {}
   def render(self, mode='human', close=False):
-    for line in self.game:
-        print(line)
-    print(" ")
-  def get_action_meanings(self):
-    return {0: 'Flip square', 1: 'Move up', 2: 'Move right', 3: 'Move down', 4: 'Move left'}
-  def clamp(self, n, minn, maxn):
-        return max(min(maxn, n), minn)
-  
+    print(self.game)
+    print(" ")  
+
+class BlackjackAgent:
+    def __init__(
+        self,
+        learning_rate: float,
+        initial_epsilon: float,
+        epsilon_decay: float,
+        final_epsilon: float,
+        discount_factor: float = 0.95,
+    ):
+        """Initialize a Reinforcement Learning agent with an empty dictionary
+        of state-action values (q_values), a learning rate and an epsilon.
+
+        Args:
+            learning_rate: The learning rate
+            initial_epsilon: The initial epsilon value
+            epsilon_decay: The decay for epsilon
+            final_epsilon: The final epsilon value
+            discount_factor: The discount factor for computing the Q-value
+        """
+        self.q_values = collections.defaultdict(lambda: np.zeros(env.action_space.n))
+
+        self.lr = learning_rate
+        self.discount_factor = discount_factor
+
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.final_epsilon = final_epsilon
+
+        self.training_error = []
+        
+
+    def get_action(self, obs: tuple[int, int, bool]) -> int:
+        """
+        Returns the best action with probability (1 - epsilon)
+        otherwise a random action with probability epsilon to ensure exploration.
+        """
+        # with probability epsilon return a random action to explore the environment
+        if np.random.random() < self.epsilon:
+            return env.action_space.sample()
+
+        # with probability (1 - epsilon) act greedily (exploit)
+        else:
+            return int(np.argmax(self.q_values[obs]))
+
+    def update(
+        self,
+        obs: tuple[int, int, bool],
+        action: int,
+        reward: float,
+        terminated: bool,
+        next_obs: tuple[int, int, bool],
+    ):
+
+        """Updates the Q-value of an action."""
+        future_q_value = (not terminated) * np.max(self.q_values[tuple(np.ndarray.flatten(next_obs))])
+        temporal_difference = (
+            reward + self.discount_factor * future_q_value - self.q_values[tuple(np.ndarray.flatten(obs))][action]
+        )
+
+        self.q_values[tuple(np.ndarray.flatten(obs))][action] = (
+            self.q_values[tuple(np.ndarray.flatten(obs))][action] + self.lr * temporal_difference
+        )
+        self.training_error.append(temporal_difference)
+
+    def decay_epsilon(self):
+        self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
+
+learning_rate = 0.01
+n_episodes = 100_000
+start_epsilon = 1.0
+epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
+final_epsilon = 0.1
+
+agent = BlackjackAgent(
+    learning_rate=learning_rate,
+    initial_epsilon=start_epsilon,
+    epsilon_decay=epsilon_decay,
+    final_epsilon=final_epsilon,
+)
+
+env = QueensEnv(4)
+
+wins = 0
 
 
+env = gymnasium.wrappers.RecordEpisodeStatistics(env, deque_size=n_episodes)
+for episode in tqdm.tqdm(range(n_episodes)):
+    obs, info = env.reset(seed=None)
+    done = False
 
-env = QueensEnv()
-obs = env.reset()
-steps = 0
-while steps <100000:
-  action = env.action_space.sample()
-  obs, reward, win, info = env.step(action)
-  env.render()
-  if steps % 1000 == 0:
-    print(reward, win)
-    time.sleep(2)
-  if win == True:
-    break
-  steps+=1
+    # play one episode
+    while not done:
+        action = agent.get_action(obs)
+        next_obs, reward, terminated, truncated, info = env.step(action)
 
-env.close()
+        # update the agent
+        agent.update(obs, action, reward, terminated, next_obs)
+
+        # update if the environment is done and the current obs
+        done = terminated or truncated
+        obs = next_obs
+
+    agent.decay_epsilon()
